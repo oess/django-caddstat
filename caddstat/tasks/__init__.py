3d66082e54fa8c0ee464ea1cce3afeefe295fe03
Craig Bruce <craig@eyesopen.com>
2013-08-29 11:37:04 -0600

    refactored file name (too short)

--- caddstat/tasks/__init__.py
+++ caddstat/tasks/__init__.py
@@ -6,7 +6,7 @@ from .mad import task_mad
 from .tau import task_tau
 from .pearson import task_pearson
 from .effect_size import task_cohen
-from .t import task_paired_t
+from .ttest import task_paired_t
 from .mannwhitney import task_mannwhitneyu
 from .friedman import task_friedman
 from .kappa import task_cohen_kappa
